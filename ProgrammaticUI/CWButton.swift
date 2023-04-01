708158788f0e5a52d4a804e06eab03d32edb91a1
Emil <42997578+emilmubarakov@users.noreply.github.com>
2023-04-01 18:11:55 +0700

    Adding buttons and image programmaticly

---
+++ ProgrammaticUI/CWButton.swift
@@ -0,0 +1,34 @@
+//
+//  CWButton.swift
+//  ProgrammaticUI
+//
+//  Created by e.mubarakov on 01.04.2023.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+        configure()
+    }
+}
